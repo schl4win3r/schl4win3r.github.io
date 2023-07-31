58a1b1e5d0ce5e3a453e5abf4c08d6a69891403a
Harald Pehl <harald.pehl@gmail.com>
2023-07-31 19:36:37 +0200

    Add Prettier & eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['RedHatText', 'sans-serif'],
+    },
   },
-  plugins: [],
-}
-
+  plugins: [require('@tailwindcss/typography')],
+};
